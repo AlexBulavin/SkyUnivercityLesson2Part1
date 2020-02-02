2c77091029f7dd5ccdaaa81c795a62ca1aeabe6f
AlexBulavin <bulavin1970@icloud.com>
2020-02-02 21:38:16 +0300

    Добавлен длинный скролируемый текст (будет потом рецептом), оформление.

--- app/src/main/java/com/example/skyunivercitylesson2part1/MainActivity.kt
+++ app/src/main/java/com/example/skyunivercitylesson2part1/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.skyunivercitylesson2part1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        recipe_text.setText.(Html.fromHtml.(findTextByID(R.string.recipe_text)))
     }
 }
